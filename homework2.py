cb1011e96840a81d2e7cf7dd381d8c935c9ef116
TortevAL <165218904+TortevAL@users.noreply.github.com>
2024-04-08 08:15:09 +0300

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -1,8 +1,8 @@
 name = "Aleksey"
 print("Name: ", name)
 age = 38
-print("age = 38")
+print("age: 38")
 age = age +1
-print("new age = " f'{age}')
+print("new age: " f'{age}')
 is_student = True
-print('is_student = True')
+print('is_student: True')
